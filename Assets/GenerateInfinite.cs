e8640fba0645b6987f94539b631e10bdbb8170f9
jdkochian <jdkochian@gmail.com>
2022-11-26 16:46:11 -0500

    an infinite sandy beach

--- Assets/GenerateInfinite.cs
+++ Assets/GenerateInfinite.cs
@@ -17,16 +17,86 @@ class Tile {
 public class GenerateInfinite : MonoBehaviour
 {
 
-    public GameObject plane; 
     public GameObject player; 
 
-    int planeSize = 10; 
-    int halfTilesX = 30; 
-    int halfTilesZ = 30; 
+    public int depth = 20;
+    public int width = 256;
+    public int height = 256;
+    public int seed = 0;
+    public int octaves = 5;
+    public float persistence = 0.5f;
+    public float lacunarity = 2f;
+    public Vector2 offset = Vector2.zero;
+    public Texture2D sandTexture;
+
+
+
+    int planeSize = 256; 
+    int halfTilesX = 2; 
+    int halfTilesZ = 2; 
 
     Vector3 startPos; 
 
     Hashtable tiles = new Hashtable();
+
+    TerrainData GenerateTerrain(TerrainData terrainData, float x_offset, float y_offset)
+    {
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(width, depth, height);
+        terrainData.SetHeights(0, 0, GenerateHeights(x_offset, y_offset));
+        return terrainData;
+    }
+
+    float[,] GenerateHeights(float x_offset, float y_offset)
+    {
+        float[,] heights = new float[width + 1, height + 1];
+
+        System.Random prng = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+
+        for (int i = 0; i < octaves; i++)
+        {
+            float offsetX = prng.Next(-100000, 100000) + offset.x;
+            float offsetY = prng.Next(-100000, 100000) + offset.y;
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
+
+
+        for (int x = 0; x <= width; x++)
+        {
+            for (int y = 0; y <= height; y++)
+            {
+                heights[x, y] = CalculateHeight(x, y, x_offset, y_offset, octaveOffsets);
+            }
+        }
+
+        return heights;
+    }
+
+    float CalculateHeight(int x, int y, float x_offset, float y_offset, Vector2[] octaveOffsets)
+    {
+
+
+        // Debug.Log(this.transform.position.x);
+        float sum_h = 0f;
+        float amplitude = 1f;
+        float frequency = 1f;
+
+        for (int i = 0; i < octaves; i++)
+        {
+            float x_coord = (float)(x + y_offset) / width * frequency + octaveOffsets[i].x;
+            float y_coord = (float)(y + x_offset) / height * frequency+ octaveOffsets[i].y;
+            // float x_coord = (float)(x ) / width * frequency + octaveOffsets[i].x;
+            // float y_coord = (float)(y ) / height * frequency+ octaveOffsets[i].y;
+            float h = Mathf.PerlinNoise(x_coord, y_coord) * 2 - 1;
+            sum_h += h * amplitude;
+
+            amplitude *= persistence;
+            frequency *= lacunarity;
+        }
+
+        return sum_h;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -40,7 +110,18 @@ public class GenerateInfinite : MonoBehaviour
         for (int x = -halfTilesX; x < halfTilesX; x++) {
             for (int z = -halfTilesZ; z < halfTilesZ; z++) { 
                 Vector3 pos = new Vector3((x * planeSize + startPos.x), 0, (z * planeSize + startPos.z));
-                GameObject t = (GameObject) Instantiate(plane, pos, Quaternion.identity);
+                TerrainData _terraindata = GenerateTerrain(new TerrainData(), pos.x, pos.z);
+
+                // sand texture
+                TerrainLayer tl = new TerrainLayer();
+                tl.diffuseTexture = sandTexture; 
+                _terraindata.terrainLayers = new TerrainLayer[] {tl};
+
+
+
+                GameObject terrain = Terrain.CreateTerrainGameObject(_terraindata);
+                GameObject t = (GameObject) Instantiate(terrain, pos, Quaternion.identity);
+                Destroy(terrain);
 
                 string tilename = "Tile_" + ((int)(pos.x)).ToString() + "_" + ((int)(pos.z)).ToString();
                 t.name = tilename; 
@@ -51,7 +132,7 @@ public class GenerateInfinite : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    // // Update is called once per frame
     void Update()
     {
 
@@ -72,7 +153,17 @@ public class GenerateInfinite : MonoBehaviour
                     string tilename = "Tile_" + ((int)(pos.x)).ToString() + "_" + ((int)(pos.z)).ToString();
 
                     if(!tiles.ContainsKey(tilename)) { 
-                        GameObject t = (GameObject) Instantiate(plane, pos, Quaternion.identity);
+                        GameObject terrain = new GameObject();
+                        TerrainData _terraindata = GenerateTerrain(new TerrainData(), pos.x, pos.z);
+                        terrain = Terrain.CreateTerrainGameObject(_terraindata);
+                         // sand texture
+                        TerrainLayer tl = new TerrainLayer();
+                        tl.diffuseTexture = sandTexture; 
+                        _terraindata.terrainLayers = new TerrainLayer[] {tl};
+
+
+                        GameObject t = (GameObject) Instantiate(terrain, pos, Quaternion.identity);
+                        Destroy(terrain);
                         t.name = tilename; 
                         Tile tile = new Tile(t, updateTime); 
                         tiles.Add(tilename, tile);
